0a44cda7b8c01532c0b57b8ced6bfd92439ef506
gustavosaid <104603397+gustavosaid@users.noreply.github.com>
2025-09-29 23:03:42 +0000

    aula sobre Docker

--- aulaSpring/CREATE TABLE aluno(.pgsql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE aluno(
-    id SERIAL PRIMARY KEY,
-    nome TEXT NOT NULL,
-    idade INTEGER NOT NULL
-)
-
-insert INTO aluno (nome,idade) VALUES ('Adriana', 51)
-
-select * from aluno
--- aulaSpring/projeto/src/main/java/com/example/projeto/model/Pessoa.java
+++ aulaSpring/projeto/src/main/java/com/example/projeto/model/Pessoa.java
@@ -1,50 +1,46 @@
-//nome 
-//idade
 package com.example.projeto.model;
-import javax.annotation.processing.Generated;
 
-import jakarta.persistenc.Entity;
-import jakarta.persistenc.Tabel;
-import jakarta.persistenc.Id;
-import jakarta.persistenc.GeneratedValue;
-import jakarta.persistenc.GeneratedType;
-
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
-@Table(name="pessoas")
+@Table(name = "pessoas")
 public class Pessoa{
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private Integer idade;
 
-    public Pessoa(){
-        this("",0);
-    }
+    public Pessoa() {}
 
     public Pessoa(String nome, Integer idade){
         this.nome = nome;
         this.idade = idade;
     }
-    
-    public void setNome(String nome){
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    public String getNome(){
-        return this.nome;
+
+    public String getNome() {
+        return nome;
     }
-    
-    public void setIdade(Integer idade){
+
+    public void setIdade(Integer idade) {
         this.idade = idade;
     }
-    public Integer getIdade(){
-        return this.idade;
+
+    public Integer getIdade() {
+        return idade;
     }
-
-
-
-
 }
